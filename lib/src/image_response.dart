a6440800e59867bd60adbabc92250cbfb4fc3a79
Lina L. <contact@lexedia.moe>
2025-12-07 15:20:48 +0100

    use jaspr proper bindings (#2)

--- lib/src/image_response.dart
+++ lib/src/image_response.dart
@@ -12,22 +12,30 @@ String mimeType(OutputFormat format) => switch (format) {
   .raw => throw UnsupportedError('raw format isnt in this context'),
 };
 
-/// An [Component] that serves an image.
-class ImageResponse extends StatelessComponent {
+/// An [AsyncStatelessComponent] that serves an image.
+///
+/// You might also be interested in [ImageResponse] for a synchronous variant.
+class ImageResponse extends AsyncStatelessComponent {
   /// The component that will be used to generate the image.
   final Component component;
 
   /// The options for generating the image.
-  final RenderOptions? options;
+  final RenderOptions options;
 
   /// Creates an [ImageResponse] with the given [component] and [options].
-  ImageResponse(this.component, {this.options});
+  const ImageResponse(
+    this.component, {
+    this.options = const RenderOptions(),
+  });
 
   @override
-  Component build(BuildContext ctx) {
-    ctx.setHeader('Content-Type', mimeType(options?.format ?? .webp));
+  Future<Component> build(BuildContext ctx) async {
+    ctx.setHeader('Content-Type', mimeType(options.format ?? .webp));
     final renderer = Renderer();
-    final bytes = renderer.renderSync(fromComponent(component), options);
+    final bytes = await renderer.render(
+      await fromComponent(component),
+      options,
+    );
     renderer.dispose();
     ctx.setStatusCode(200, responseBody: bytes);
 
